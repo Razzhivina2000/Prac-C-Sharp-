6e1c52d8c31793a6e264b73dd0ebc82b41e51e36
Razzhivina2000 <72811486+Razzhivina2000@users.noreply.github.com>
2020-11-23 23:00:54 +0300

    Add files via upload

---
+++ Lab2/DataItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    public struct DataItem
+    {
+        public float t { get; set; }
+        public Vector3 vec { get; set; }
+        public DataItem(float t, Vector3 vec)
+        {
+            this.t = t;
+            this.vec = vec;
+        }
+        public override string ToString() => $"{t} {vec.ToString()}";
+        public string ToString(string format) => $"{t.ToString(format)} {vec.ToString(format)} {vec.Length().ToString(format)}";
+    }
+}
